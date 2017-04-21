0a19cba85db7b0701f7da786f5bfcd111008d347
869755676@qq.com <869755676@qq.com>
2017-04-21 14:20:01 +0800

    图片+toolbar的使用

--- app/src/main/java/com/example/recyclerdirection/testmaterialdesign/MainActivity.java
+++ app/src/main/java/com/example/recyclerdirection/testmaterialdesign/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.recyclerdirection.testmaterialdesign;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void bar(View v) {
+        startActivity(new Intent(this,ToolBarActivity.class));
+
     }
 }
---
+++ app/src/main/java/com/example/recyclerdirection/testmaterialdesign/ToolBarActivity.java
@@ -0,0 +1,52 @@
+package com.example.recyclerdirection.testmaterialdesign;
+
+import android.app.Activity;
+import android.graphics.Outline;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.widget.TextView;
+
+public class ToolBarActivity extends Activity {
+
+    private CollapsingToolbarLayout toolbarLayout;
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar);
+        toolbarLayout= (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
+        recyclerView= (RecyclerView) findViewById(R.id.recyclerView);
+        toolbarLayout.setTitle("imageView");
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerView.Adapter<ViewHolder>() {
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new ViewHolder(getLayoutInflater().inflate(R.layout.item_layout,null));
+            }
+
+            @Override
+            public void onBindViewHolder(ViewHolder holder, int position) {
+                 holder.tv.setText("位置位置"+position);
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+        });
+
+
+    }
+    private class  ViewHolder extends RecyclerView.ViewHolder{
+              TextView tv;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tv= (TextView) itemView.findViewById(R.id.tv);
+        }
+    }
+}
