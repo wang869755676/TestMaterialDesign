4dcfeca165aaaf0b636614bb0308d6d77f88b216
wangjinc <3049573633@qq.com>
2018-08-15 13:35:39 +0800

    materdesigin的处理

--- app/src/main/java/com/example/recyclerdirection/testmaterialdesign/Fragment_a.java
+++ app/src/main/java/com/example/recyclerdirection/testmaterialdesign/Fragment_a.java
@@ -5,15 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 public class Fragment_a extends Fragment {
 
+    private WebView webView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment, container, false);
-
+        webView = (WebView) view.findViewById(R.id.web);
+        webView.getSettings().setBlockNetworkImage(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY); //取消滚动条白边效果
+        webView.loadUrl("https://blog.csdn.net/kilotwo/article/details/79197498");
 
         return view;
     }
